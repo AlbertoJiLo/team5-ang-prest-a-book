df11cc72fbd9c109af9588f79bfbd7389b14640f
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-12-11 21:26:12 +0100

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { BooksearchComponent } from './booksearch/booksearch.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { InterfaceComponent } from './interface/interface.component';
 import { LoginregisterComponent } from './loginregister/loginregister.component';
 import { AboutComponent } from './about/about.component';
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     BooksearchComponent,
-    HeaderComponent,
-    FooterComponent,
     InterfaceComponent,
     LoginregisterComponent,
     AboutComponent,
@@ -27,7 +23,7 @@ import { AboutComponent } from './about/about.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    HttpClientModule
+    ElementsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/loginregister/loginregister.component.ts
+++ src/app/loginregister/loginregister.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-loginregister',
   templateUrl: './loginregister.component.html',
   styleUrls: ['./loginregister.component.css']
 })
 export class LoginregisterComponent {
+
+  register: boolean = false;
+
+  constructor () {
+
+  }
+
+  cambiarEstado() {
+
+    this.register = this.register?false:true;
+
+  }
 
 }
